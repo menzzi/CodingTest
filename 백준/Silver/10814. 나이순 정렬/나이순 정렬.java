6e106940bf659eb9c5a12f6e940e7d2c6c455ea2
menzzi <124131845+menzzi@users.noreply.github.com>
2023-04-14 19:40:42 +0900

    [Silver V] Title: 나이순 정렬, Time: 716 ms, Memory: 50856 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -3,7 +3,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Main{
@@ -12,43 +11,30 @@ public class Main{
 
         int N = Integer.parseInt(br.readLine());
 
-        Person[] p = new Person[N];
+        String arr[][] = new String[N][2];
 
         for(int i=0;i<N;i++){
             StringTokenizer st = new StringTokenizer(br.readLine()," ");
-            int age = Integer.parseInt(st.nextToken());
-            String name = st.nextToken();
-            p[i] = new Person(age,name);
+            arr[i][0] = st.nextToken();
+            arr[i][1] = st.nextToken();
         }
 
-        Arrays.sort(p, new Comparator<Person>() {
-            @Override
-            public int compare(Person o1, Person o2) {
-                return o1.age-o2.age;
-            }
-        });
+
+       Arrays.sort(arr, new Comparator<String[]>() {
+           @Override
+           public int compare(String[] o1, String[] o2) {
+               return Integer.parseInt(o1[0])-Integer.parseInt(o2[0]);
+           }
+       });
 
 
         StringBuilder sb = new StringBuilder();
         for(int i=0;i<N;i++){
-            sb.append(p[i]);
+            sb.append(arr[i][0]).append(' ').append(arr[i][1]).append('\n');
         }
         System.out.println(sb);
     }
 
-    public static class Person{
-        int age;
-        String name;
-        public Person(int age,String name){
-            this.age = age;
-            this.name = name;
-        }
-
-        @Override
-        public String toString() {
-            return age + " " + name + "\n";
-        }
-    }
 }
 
 
