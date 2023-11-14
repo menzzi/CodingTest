bcb906ba80d09329a36e61ef41285018689458bc
menzzi <124131845+menzzi@users.noreply.github.com>
2023-11-14 16:48:00 +0900

    [level 1] Title: 최소직사각형, Time: 1.72 ms, Memory: 80.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int solution(int[][] sizes) {
+        int answer = 0;
+        for(int i =0;i<sizes.length;i++){
+            if(sizes[i][0]<sizes[i][1]){
+                int tmp = sizes[i][0];
+                sizes[i][0] = sizes[i][1];
+                sizes[i][1] = tmp;
+            }
+        }
+        int max1 = 0;
+        int max2 = 0;
+        for(int i=0;i<sizes.length;i++){
+            if(max1<sizes[i][0]){
+                max1=sizes[i][0];
+            }
+            if(max2<sizes[i][1]){
+                max2=sizes[i][1];
+            }
+        }
+        answer = max1 * max2;
+        return answer;
+    }
+}
