99f734605c6079d09bc0b9c03a58fdef007c6cee
menzzi <124131845+menzzi@users.noreply.github.com>
2024-01-06 12:34:28 +0900

    [level 3] Title: N으로 표현, Time: 0.02 ms, Memory: 71.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class Solution {
+    public int solution(int N, int number) {
+        if(N==number) return 1;
+        
+        List<Set<Integer>> dp = new ArrayList<>();
+        for(int i=0;i<=8;i++){
+            dp.add(new TreeSet<>());
+        }
+        dp.get(1).add(N);
+        for(int i=2;i<=8;i++){
+            
+            dp.get(i).add(Integer.parseInt(Integer.toString(N).repeat(i)));
+            
+            for(int j=1;j<i;j++){
+                int k = i-j;
+                for(int num1:dp.get(j)){
+                    for(int num2:dp.get(k)){
+                        dp.get(i).add(num1+num2);
+                        dp.get(i).add(num1-num2);
+                        dp.get(i).add(num1*num2);
+                        if(num2!=0)dp.get(i).add(num1/num2);
+                    }
+                }
+            }
+            if(dp.get(i).contains(number))return i;
+        }
+        
+        return -1;
+    }
+}
