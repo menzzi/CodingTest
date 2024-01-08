2da01ee49f645f9237190783fb710e23d1ccc746
menzzi <124131845+menzzi@users.noreply.github.com>
2024-01-08 15:00:09 +0900

    [level 2] Title: 의상, Time: 0.09 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,15 +1,14 @@
-import java.util.HashMap;
+import java.util.*;
 
 class Solution {
     public int solution(String[][] clothes) {
         int answer = 1;
-        HashMap<String,Integer> map = new HashMap();
-        
-        for(int i=0; i<clothes.length;i++){
-            map.put(clothes[i][1],map.getOrDefault(clothes[i][1],0)+1);
+        HashMap<String,Integer> map = new HashMap<>();
+        for(int i=0;i<clothes.length;i++){
+            map.put(clothes[i][1],map.getOrDefault(clothes[i][1],1)+1);
         }
-        for(HashMap.Entry<String,Integer> entry : map.entrySet()){
-            answer*= (entry.getValue()+1);
+        for(String key:map.keySet()){
+            answer *= map.get(key);
         }
         return answer-1;
     }
