9d0e117160b921bf42f0e824ce81f26c47e656f5
menzzi <124131845+menzzi@users.noreply.github.com>
2024-01-08 13:58:18 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 64.14 ms, Memory: 95.4 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,20 +1,21 @@
-import java.util.HashMap;
+import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
-        HashMap<String,Integer> map = new HashMap();
+        HashMap<String,Integer> map = new HashMap<>();
+        
         for(int i=0;i<participant.length;i++){
             map.put(participant[i],map.getOrDefault(participant[i],0)+1);
         }
         for(int i=0;i<completion.length;i++){
             map.put(completion[i],map.get(completion[i])-1);
         }
-        for(int i=0;i<participant.length;i++){
-            if(map.get(participant[i])!=0) return participant[i];
+        for(String key:map.keySet()){
+            if(map.get(key)!=0){
+                return key;
+            }
         }
-        
-        
         return answer;
     }
 }
