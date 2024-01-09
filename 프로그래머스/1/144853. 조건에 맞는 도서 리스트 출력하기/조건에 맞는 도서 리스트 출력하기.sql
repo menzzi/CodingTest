73041660469e09bcfb32371ea032fe8266f72fd7
menzzi <124131845+menzzi@users.noreply.github.com>
2024-01-09 09:36:05 +0900

    [level 1] Title: 조건에 맞는 도서 리스트 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/144853. 조건에 맞는 도서 리스트 출력하기/조건에 맞는 도서 리스트 출력하기.sql
@@ -0,0 +1,5 @@
+select BOOK_ID, DATE_FORMAT(PUBLISHED_DATE,'%Y-%m-%d') as PUBLISHED_DATE
+from BOOK
+where YEAR(PUBLISHED_DATE) = '2021' and CATEGORY = '인문'
+group by BOOK_ID
+order by PUBLISHED_DATE 
