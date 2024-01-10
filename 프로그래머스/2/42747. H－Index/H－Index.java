6e5af2aaf719a7194aebd8d0a830306d69f95522
menzzi <124131845+menzzi@users.noreply.github.com>
2024-01-10 15:02:29 +0900

    [level 2] Title: H-Index, Time: 0.54 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,17 +1,19 @@
 import java.util.*;
-
 class Solution {
     public int solution(int[] citations) {
         int answer = 0;
         Arrays.sort(citations);
-        
-        for(int i=0;i<citations.length;i++){
-            if(citations[i]>=citations.length-i){
-                answer = citations.length - i;
-                break;
+        int h = citations[citations.length-1];
+        while(h>=0){
+            int count = 0;
+            for(int i=0;i<citations.length;i++){
+                if(citations[i]>=h)count++;
             }
+            if(count<h){
+                h--;
+            }else if(count>=h)break;
         }
-        
+        answer = h;
         return answer;
     }
 }
