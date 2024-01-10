5ca2bd191cf1c204bfcd3741eae7e5110741eb6f
menzzi <124131845+menzzi@users.noreply.github.com>
2024-01-10 12:58:53 +0900

    [level 2] Title: 가장 큰 수, Time: 1.62 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -2,20 +2,23 @@ import java.util.*;
 
 class Solution {
     public String solution(int[] numbers) {
-        
-        String[] answer = new String[numbers.length];
+        String answer = "";
+        String[] arr = new String[numbers.length];
         for(int i=0;i<numbers.length;i++){
-            answer[i] = String.valueOf(numbers[i]);
+            arr[i] = Integer.toString(numbers[i]);
         }
-        Arrays.sort(answer,(o1,o2)->(o2+o1).compareTo(o1+o2));
-        
-        if(answer[0].equals("0"))return "0";
-        
+        Arrays.sort(arr,new Comparator<String>(){
+            @Override
+            public int compare(String num1,String num2){
+                return (num2+num1).compareTo(num1+num2);                
+            }
+        });
         StringBuilder sb = new StringBuilder();
-        for(String s:answer){
-            sb.append(s);
+        for(int i=0;i<numbers.length;i++){
+            sb.append(arr[i]);
         }
-       
-        return String.valueOf(sb);
+        answer = sb.toString();
+        if(answer.startsWith("0")) return "0";
+        return answer;
     }
 }
