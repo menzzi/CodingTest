8716f61ff54dd3f4e0a19f4459cc313be58e057c
menzzi <124131845+menzzi@users.noreply.github.com>
2024-01-10 11:00:55 +0900

    [level 1] Title: K번째수, Time: 0.97 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -3,17 +3,11 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
-        
         for(int i=0;i<commands.length;i++){
-            int num = commands[i][1]-commands[i][0]+1;
-            int[] input = new int[num];
-            for(int j=0;j<num;j++){
-                input[j] = array[j+commands[i][0]-1];
-            }
-            Arrays.sort(input);
-            answer[i] = input[commands[i][2]-1];
+            int[] arr = Arrays.copyOfRange(array,commands[i][0]-1,commands[i][1]);
+            Arrays.sort(arr);        
+            answer[i] = arr[commands[i][2]-1];
         }
-        
         return answer;
     }
 }
