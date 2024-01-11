6d213b1d4f47f0c52887d6d6aae79cc6695e1c36
menzzi <124131845+menzzi@users.noreply.github.com>
2024-01-11 14:37:39 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.50 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -3,36 +3,32 @@ import java.util.*;
 class Solution {
     public int[] solution(String[] operations) {
         int[] answer = new int[2];
-        PriorityQueue<Integer> max = new PriorityQueue<>(Collections.reverseOrder());
-        PriorityQueue<Integer> min = new PriorityQueue<>();
+        PriorityQueue<Integer> maxq = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> minq = new PriorityQueue<>();
         
         for(int i=0;i<operations.length;i++){
-            String s = operations[i];
-            StringTokenizer st = new StringTokenizer(s," ");
-            String O = st.nextToken();
-            int num = Integer.parseInt(st.nextToken());
+            String[] str = operations[i].split(" ");
+            int num = Integer.parseInt(str[1]);
             
-            if(O.equals("I")){
-                max.offer(num);
-                min.offer(num);
-            }else if(O.equals("D")){
-                if(max.isEmpty())continue;
+            if(str[0].equals("I")){
+                maxq.add(num);
+                minq.add(num);
+            }else{
                 if(num == 1){
-                    int del = max.poll();
-                    min.remove(del);
-                }else if(num == -1){
-                    int del = min.poll();
-                    max.remove(del);
+                    minq.remove(maxq.poll());
+                }else{
+                    maxq.remove(minq.poll());
                 }
             }
         }
-        if(!max.isEmpty()){
-            answer[0] = max.poll();
-            answer[1] = min.poll();
-        }else{
+        if(!maxq.isEmpty()&&!minq.isEmpty()){
+            answer[0] = maxq.poll();
+            answer[1] = minq.poll();
+        }else if(maxq.size()==0 && minq.size()==0){
             answer[0] = 0;
             answer[1] = 0;
         }
+        
         return answer;
     }
 }
