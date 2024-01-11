2b4faffd1ecb6dd361eed025b7e7eeb5359be859
menzzi <124131845+menzzi@users.noreply.github.com>
2024-01-11 15:36:47 +0900

    [level 2] Title: 기능개발, Time: 0.12 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -4,27 +4,31 @@ class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
         
         Queue<Integer> q = new LinkedList<>();
-        List<Integer> a = new ArrayList<>();
+        Queue<Integer> aq = new LinkedList<>();
         
         for(int i=0;i<progresses.length;i++){
             int day = 0;
-            while(progresses[i]+speeds[i]*day<100){
-                day++;
+            while(progresses[i]+speeds[i]*day <100){
+                day+=1;
             }
-            q.offer(day);
+            q.add(day);
         }
         
         while(!q.isEmpty()){
-            int num = q.poll();
             int cnt = 1;
-            while(!q.isEmpty() && q.peek()<=num){
+            int p = q.poll();
+            while(!q.isEmpty() && q.peek()<=p){
                 q.poll();
-                cnt++;
+                cnt+=1;
             }
-            a.add(cnt);
+            aq.add(cnt);
         }
-       
-        
-        return a.stream().mapToInt(Integer::intValue).toArray(); 
+        int[] answer = new int[aq.size()];
+        int idx = 0;
+        while(!aq.isEmpty()){
+            answer[idx] = aq.poll();
+            idx+=1;
+        }
+        return answer;
     }
 }
