bf44d42f66cc138045bbaefde888605c7178c5e8
menzzi <124131845+menzzi@users.noreply.github.com>
2024-01-11 11:51:22 +0900

    [level 2] Title: 더 맵게, Time: 1387.81 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/README.md
+++ 프로그래머스/2/42626. 더 맵게/README.md
@@ -1,10 +1,10 @@
 # [level 2] 더 맵게 - 42626 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626#) 
 
 ### 성능 요약
 
-메모리: 123 MB, 시간: 1770.73 ms
+메모리: 123 MB, 시간: 1387.81 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 6일 16:43:48
+2024년 1월 4일 11:51:20
 
 ### 문제 설명
 
--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -4,22 +4,19 @@ class Solution {
     public int solution(int[] scoville, int K) {
         int answer = 0;
         PriorityQueue<Integer> q = new PriorityQueue<>();
-        
         for(int i=0;i<scoville.length;i++){
             q.offer(scoville[i]);
         }
-        
         while(q.peek()<K){
-            if(q.size()==1){
-                return -1;
-            }
-            int a = q.poll();
-            int b = q.poll();
-            int num = a + b * 2;
+            if(q.size() == 1) return -1;
+            int num1 = q.poll();
+            if(num1 >= K) break;
+            int num2 = q.poll();
+            
+            int num3 = num1 + num2 *2;
+            q.offer(num3);
             answer++;
-            q.offer(num);
         }
-        
         return answer;
     }
 }
