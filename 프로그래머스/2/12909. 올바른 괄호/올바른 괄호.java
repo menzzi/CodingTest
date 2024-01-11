af475ab9414b0f4086b761e060f925a53a6b7dba
menzzi <124131845+menzzi@users.noreply.github.com>
2024-01-11 16:23:41 +0900

    [level 2] Title: 올바른 괄호, Time: 16.37 ms, Memory: 52.6 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -2,25 +2,19 @@ import java.util.*;
 
 class Solution {
     boolean solution(String s) {
-        boolean answer = true;
-        Stack<Integer> stack = new Stack<>();
-        if(s.charAt(0)==')')return false;
-        
+        boolean answer = false;
+
+        Stack<Integer> st = new Stack<>();
         for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='(')stack.push(1);
-            else{
-                if (stack.isEmpty()) {
-                        answer = false;
-                        break;
-                    } else {
-                        stack.pop();
-                    }
+            char ch = s.charAt(i);
+            if(ch == '('){
+                st.push(1);
+            }else{
+                if(st.isEmpty())return false;
+                else st.pop();
             }
-            
         }
-        
-        if(stack.size()>0)answer = false;
-        
+        if(st.isEmpty())return true;
         return answer;
     }
 }
