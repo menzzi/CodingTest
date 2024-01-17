168373ef9fc8e5c4c35f35d6e50de72e314708c0
menzzi <124131845+menzzi@users.noreply.github.com>
2024-01-17 15:26:31 +0900

    [level 1] Title: 체육복, Time: 0.02 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,45 +1,27 @@
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        int answer = 0;
-        int[] count = new int[n+1];
-        for(int i=1;i<=n;i++){
-            count[i] = 1;
-        }
+        int answer = n;
+        int[] st = new int[n+1];
         for(int i=0;i<lost.length;i++){
-            count[lost[i]] -= 1;
+            st[lost[i]] -= 1;
         }
         for(int i=0;i<reserve.length;i++){
-            count[reserve[i]] += 1;
+            st[reserve[i]] +=1;
         }
-        
-        if(count[1]==0){
-            if(count[2]==2){
-                count[2]=1;
-                answer++;
-            }
-        }else if(count[1]==1||count[1]==2)answer++;
-            
-        for(int i=2;i<n;i++){
-            if(count[i]==1||count[i]==2)answer++;
-            else if(count[i]==0){
-                if(count[i-1]==2){
-                    count[i-1]=1;
-                    answer++;
-                }else if(count[i+1]==2){
-                    count[i+1]=1;
-                    answer++;
+        for(int i=1;i<n;i++){
+            if(st[i] == -1){
+                if(st[i-1] == 1 && i-1 != 0){
+                    st[i-1] -=1;
+                }else if(st[i+1] == 1 ){
+                    st[i+1] -=1;
+                }else{
+                    answer -=1;
                 }
             }
         }
-  
-        
-        if(count[n]==0){
-            if(count[n-1]==2){
-                count[n-1]=1;
-                answer++;
-            }
-        }else if(count[n]==1||count[n]==2)answer++;
-        
+        if(st[n]==-1){
+            if(st[n-1]!=1) answer-=1;
+        }
         return answer;
     }
 }
