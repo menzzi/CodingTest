855b87f32dbfc0eabc9bcd5c7af51bf3e4d92ea9
menzzi <124131845+menzzi@users.noreply.github.com>
2024-01-17 15:04:34 +0900

    [level 4] Title: 징검다리, Time: 28.03 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/4/43236. 징검다리/징검다리.java
+++ 프로그래머스/4/43236. 징검다리/징검다리.java
@@ -3,30 +3,30 @@ import java.util.*;
 class Solution {
     public int solution(int distance, int[] rocks, int n) {
         int answer = 0;
-        int right = distance;
-        int left = 0;
-        
         Arrays.sort(rocks);
         
-        while(left<=right){
+        int left = 0;
+        int right = distance;
+        
+        while(left <= right){
             int mid = (left+right)/2;
             int remove = 0;
             int pre = 0;
             
             for(int i=0;i<rocks.length;i++){
-                if(rocks[i]-pre<mid){
-                    remove++;
-                }else pre = rocks[i];
-                if(remove>n)break;
+                if(rocks[i]-pre<mid)remove++;
+                else pre = rocks[i];
+                
+                if(remove>n) break;
             }
-            if(distance - pre < mid && remove<=n)remove++;
+            if(distance-pre < mid && remove<=n)remove++;
             
-            if(remove>n) right = mid -1;
-            else{
-                left = mid+1;
+            if(remove>n){
+                right = mid - 1;
+            }else{
                 answer = mid;
-            } 
-            
+                left = mid + 1;
+            }
         }
         return answer;
     }
