445f3cefaa5dc7f5246d09e810ad7f712ec5f1af
menzzi <124131845+menzzi@users.noreply.github.com>
2024-01-18 13:35:34 +0900

    [level 2] Title: 조이스틱, Time: 0.03 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -1,26 +1,22 @@
-import java.lang.Math;
+import java.util.*;
 class Solution {
     public int solution(String name) {
+        
         int answer = 0;
         int move = name.length()-1;
-        int[] count = new int[name.length()];
+        
         for(int i=0;i<name.length();i++){
-            char c = name.charAt(i);
-            if(c > 'M') count[i]= 'Z'-c +1;
-            else count[i] = c-'A';
+            if(name.charAt(i) > 'M') answer += 'Z' - name.charAt(i) +1;
+            else answer += name.charAt(i) - 'A';
+            
+            int idx = i+1;
+            while(idx < name.length() && name.charAt(idx) == 'A') idx++;
+            
+            move = Math.min(move,i*2+name.length()-idx);
+            move = Math.min(move,(name.length()-idx)*2 + i);
+            
         }
-
-        for(int i=0;i<name.length();i++){
-            answer +=count[i];
-            int index = i+1;
-            while(index<name.length()&&count[index]==0){
-                index++;
-            }
-            move = Math.min(move,i*2+name.length()-index);
-            move = Math.min(move,(name.length()-index)*2+i);
-        }
-      
-        answer+=move;
+        answer += move;
         return answer;
     }
 }
