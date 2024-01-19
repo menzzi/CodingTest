55dde89bcc6607a613784a0dd0222fa391c70839
menzzi <124131845+menzzi@users.noreply.github.com>
2024-01-19 12:34:29 +0900

    [level 3] Title: 가장 먼 노드, Time: 890.41 ms, Memory: 630 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -2,38 +2,35 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[][] edge) {
+        int answer = 0;
+        boolean[][] map = new boolean[n+1][n+1];
         int[] count = new int[n+1];
-        boolean[][] node = new boolean[n+1][n+1];
         
         for(int i=0;i<edge.length;i++){
-            node[edge[i][0]][edge[i][1]] = true;
-            node[edge[i][1]][edge[i][0]] = true;
+            map[edge[i][0]][edge[i][1]] = true;
+            map[edge[i][1]][edge[i][0]] = true;
         }
         
         Queue<Integer> q = new LinkedList<>();
         q.add(1);
-        
         while(!q.isEmpty()){
-            int poll = q.remove();
-            
-            for(int i=2;i<=n;i++){
-                if(node[poll][i] && count[i]==0){
-                    count[i] = count[poll] +1;
+            int poll = q.poll();
+            for(int i=2;i<n+1;i++){
+                if(map[poll][i] && count[i]==0){
+                    count[i] = count[poll]+1;
                     q.add(i);
                 }
             }
         }
         int max = 0;
-        int answer = 0;
-        for(int i=1;i<=n;i++){
+        for(int i=0;i<n+1;i++){
             if(max<count[i]){
                 max = count[i];
-                answer=1;
-            }else if(max==count[i]){
+                answer = 1;
+            }else if(max == count[i]){
                 answer++;
             }
         }
-        
         return answer;
     }
 }
