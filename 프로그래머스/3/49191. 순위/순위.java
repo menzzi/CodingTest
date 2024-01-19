744eadfc2e2b8263ea02ea2913111a785b781e16
menzzi <124131845+menzzi@users.noreply.github.com>
2024-01-19 13:06:57 +0900

    [level 3] Title: 순위, Time: 14.93 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.java
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -1,36 +1,34 @@
+import java.util.*;
+
 class Solution {
     public int solution(int n, int[][] results) {
         int answer = 0;
-        int[][] board = new int[n+1][n+1];
-        
+        int[][] win = new int[n+1][n+1];
         for(int i=0;i<results.length;i++){
-            board[results[i][0]][results[i][1]] = 1;
-            board[results[i][1]][results[i][0]] = -1;
+            win[results[i][0]][results[i][1]] = 1;
+            win[results[i][1]][results[i][0]] = -1;
         }
-        
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n;j++){
                 for(int k=1;k<=n;k++){
-                    if(board[i][j]==1 && board[j][k]==1){
-                        board[i][k] = 1;
-                        board[k][i] = -1;
+                    if(win[i][j]==1 && win[j][k]==1){
+                        win[i][k] = 1;
+                        win[k][i] = -1;
                     }
-                    if(board[i][j]==-1 && board[j][k]==-1){
-                        board[i][k] = -1;
-                        board[k][i] = 1;
+                    if(win[i][j] == -1 &&win[j][k]==-1){
+                        win[i][k] = -1;
+                        win[k][i] = 1;
                     }
                 }
             }
         }
-        
         for(int i=1;i<=n;i++){
             int cnt = 0;
             for(int j=1;j<=n;j++){
-                if(board[i][j]==0)cnt++;
+                if(win[i][j]!=0)cnt++;
             }
-            if(cnt==1)answer++;
+            if(cnt == n-1)answer++;
         }
-        
         return answer;
     }
 }
