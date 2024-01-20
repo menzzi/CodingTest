2e5cde09091f72d954a1589aca7492e4c26a8e46
menzzi <124131845+menzzi@users.noreply.github.com>
2024-01-20 13:54:01 +0900

    [level 3] Title: 섬 연결하기, Time: 0.43 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,34 +1,34 @@
 import java.util.*;
+
 class Solution {
     static int[] parent;
-    
     public int solution(int n, int[][] costs) {
         int answer = 0;
-        Arrays.sort(costs,(int[] c1,int[] c2)-> c1[2]-c2[2]);
-        
         parent = new int[n];
-        for(int i=0;i<n;i++){
-            parent[i]=i;
-        }
         
+        Arrays.sort(costs,(int[] c1,int[] c2)->(c1[2]-c2[2]));
+        
+        for(int i=0;i<n;i++){
+            parent[i] = i;
+        }
         for(int i=0;i<costs.length;i++){
             int from = costs[i][0];
             int to = costs[i][1];
             int cost = costs[i][2];
             
-            int fromParent = findParent(from);
-            int toParent = findParent(to);
+            int fromP = findParent(from);
+            int toP = findParent(to);
             
-            if(fromParent == toParent) continue;
+            if(fromP == toP) continue;
             
-            answer +=cost;
-            parent[toParent] = fromParent;
+            parent[toP] = fromP;
+            answer += cost;
         }
+        
         return answer;
     }
-    
     public int findParent(int child){
-        if(parent[child]==child)return parent[child];
-        return parent[child] = findParent(parent[child]);
+        if(parent[child] == child) return parent[child];
+        else return parent[child] = findParent(parent[child]);
     }
 }
