fc8b8b4f18faa41899bcf3e90838f536ea809090
menzzi <124131845+menzzi@users.noreply.github.com>
2024-01-20 14:10:45 +0900

    [level 3] Title: 단속카메라, Time: 8.08 ms, Memory: 58.3 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -1,18 +1,17 @@
+
 import java.util.*;
 
 class Solution {
     public int solution(int[][] routes) {
-        int answer = 0;
+        int answer = 1;
         Arrays.sort(routes,(int[] r1,int[] r2)->r1[1]-r2[1]);
+        int out = routes[0][1];
         
-        int current = -30001;
-        for(int i=0;i<routes.length;i++){
-            if(current < routes[i][0]){
-                answer++;
-                current = routes[i][1];
-            }
+        for(int i=1;i<routes.length;i++){
+            if(out >= routes[i][0]) continue;
+            out = routes[i][1];
+            answer++;
         }
-        
         return answer;
     }
 }
