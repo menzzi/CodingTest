401d35b4cd8a700c2dee54a7e0fb1bea5506067a
menzzi <124131845+menzzi@users.noreply.github.com>
2024-01-21 14:07:17 +0900

    [level 3] Title: 등굣길, Time: 1.00 ms, Memory: 52.5 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,25 +1,30 @@
+import java.util.*;
+
 class Solution {
     public int solution(int m, int n, int[][] puddles) {
+        int answer = 0;
+        int[][] map = new int[n+1][m+1];
         
-        int[][] dp = new int[n+1][m+1];
         for(int i=0;i<puddles.length;i++){
-            dp[puddles[i][1]][puddles[i][0]] = -1;
+            map[puddles[i][1]][puddles[i][0]] = -1;
         }
         
-        dp[1][1] = 1;
+        map[1][1] = 1;
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
-                if(dp[i][j]==0){
-                    if(i!=1 && dp[i-1][j]>=0){
-                        dp[i][j] += dp[i-1][j];
+                if(map[i][j]==0){
+                    if(j-1 > 0 && map[i][j-1] > 0){
+                        map[i][j] += map[i][j-1];
+                        map[i][j] %= 1000000007;
                     }
-                    if(j!=1 && dp[i][j-1]>=0){
-                        dp[i][j] += dp[i][j-1];
+                    if(i-1>0 && map[i-1][j] > 0){
+                        map[i][j] += map[i-1][j];
+                        map[i][j] %= 1000000007;
                     }
-                    dp[i][j] %= 1000000007;
                 }
             }
         }
-        return dp[n][m];
+        answer = map[n][m];
+        return answer;
     }
 }
