2be76e409a15e37cdf3b6f2966b44903caef60e3
menzzi <124131845+menzzi@users.noreply.github.com>
2024-02-19 13:06:35 +0900

    [level 4] Title: 그룹별 조건에 맞는 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
+++ 프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
@@ -1,14 +1,11 @@
 -- 코드를 입력하세요
-SELECT MEMBER_NAME,REVIEW_TEXT,date_format(REVIEW_DATE,'%Y-%m-%d') as REVIEW_DATE
-from MEMBER_PROFILE p join 
-(select MEMBER_ID,REVIEW_TEXT,REVIEW_DATE
-from REST_REVIEW
-where MEMBER_ID = (
-        select MEMBER_ID
-        from REST_REVIEW
-        group by MEMBER_ID
-        order by count(*) desc
-        limit 1
-    )
-) r on p.MEMBER_ID=r.MEMBER_ID
-order by REVIEW_DATE,REVIEW_TEXT
+SELECT MEMBER_NAME,REVIEW_TEXT,DATE_FORMAT(REVIEW_DATE,'%Y-%m-%d') AS REVIEW_DATE
+FROM MEMBER_PROFILE m JOIN (SELECT MEMBER_ID,REVIEW_TEXT,REVIEW_DATE
+                            FROM REST_REVIEW
+                            WHERE MEMBER_ID = (SELECT MEMBER_ID 
+                                               FROM REST_REVIEW 
+                                               GROUP BY MEMBER_ID
+                                               ORDER BY count(*) DESC
+                                               limit 1)
+                           ) r on m.MEMBER_ID=r.MEMBER_ID
+ORDER BY REVIEW_DATE,REVIEW_TEXT
