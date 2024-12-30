2ede1410f16f45c5d316f83cdb15f1b509dea1dd
menzzi <124131845+menzzi@users.noreply.github.com>
2024-12-30 18:14:21 +0900

    [level 1] Title: 체육복, Time: 0.03 ms, Memory: 87.9 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,27 +1,32 @@
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
         int answer = n;
-        int[] st = new int[n+1];
-        for(int i=0;i<lost.length;i++){
-            st[lost[i]] -= 1;
+        int[] student = new int[n];
+        
+        for(int lostIndex : lost){
+            student[lostIndex - 1]--;
         }
-        for(int i=0;i<reserve.length;i++){
-            st[reserve[i]] +=1;
+        
+        for(int reserveIndex : reserve){
+            student[reserveIndex - 1]++;
         }
-        for(int i=1;i<n;i++){
-            if(st[i] == -1){
-                if(st[i-1] == 1 && i-1 != 0){
-                    st[i-1] -=1;
-                }else if(st[i+1] == 1 ){
-                    st[i+1] -=1;
-                }else{
-                    answer -=1;
+        
+        for(int i = 0;i < n;i++){
+            if(student[i] == -1){
+                if(i - 1 >= 0 && student[i-1] > 0){
+                    student[i]++;
+                    student[i-1]--;
+                }else if(i + 1 < n && student[i+1] > 0){
+                    student[i]++;
+                    student[i+1]--;
                 }
             }
         }
-        if(st[n]==-1){
-            if(st[n-1]!=1) answer-=1;
+     
+        for(int i = 0;i < n;i++){
+            if(student[i] == -1) answer--;
         }
+            
         return answer;
     }
 }
