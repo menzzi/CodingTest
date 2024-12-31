717027c0d0441b8fbad30651faff3a777e7139a8
menzzi <124131845+menzzi@users.noreply.github.com>
2024-12-31 13:56:42 +0900

    [level 2] Title: 큰 수 만들기, Time: 7967.09 ms, Memory: 86.7 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -2,24 +2,29 @@ import java.util.*;
 
 class Solution {
     public String solution(String number, int k) {
-        String answer = "";
-        Stack<Character> st = new Stack<>();
-        int num = k;
-        for(int i=0;i<number.length();i++){
-            char c = number.charAt(i);
-            while(!st.isEmpty() && st.peek()<c && k > 0){
-                st.pop();
-                k-=1;
+        StringBuilder answer = new StringBuilder();
+        int maxNumIndex = 0;
+        int lastIndex = k;
+        
+        while(answer.length() < number.length()-k){
+            char max = Character.MIN_VALUE;
+            int maxIndex = 0;
+            for(int i = maxNumIndex; i <= lastIndex; i++){
+                char num = number.charAt(i);
+                if(max < num){
+                    max = num;
+                    maxIndex = i;
+                }
             }
-            
-            st.push(c);
+            if(maxIndex == lastIndex) {
+                answer.append(number.substring(lastIndex));
+                return answer.toString();
+            }
+            answer.append(number.charAt(maxIndex));
+            maxNumIndex = maxIndex + 1;
+            lastIndex++;
         }
-        
-        for(int i=0;i<number.length()-num;i++){
-            answer += st.get(i);
-        }
-        
-        
-        return answer;
+    
+        return answer.toString();
     }
 }
