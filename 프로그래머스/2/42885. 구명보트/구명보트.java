bd58802275239aca02666729b016c660cf3b7b67
menzzi <124131845+menzzi@users.noreply.github.com>
2024-12-31 14:34:53 +0900

    [level 2] Title: 구명보트, Time: 10.59 ms, Memory: 57.9 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -4,15 +4,14 @@ class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
         Arrays.sort(people);
-        int j=0;
-        for(int i = people.length-1; i>=j;i--){
+        
+        int j = 0;
+        for(int i = people.length - 1; i > j; i--){
             if(people[i] + people[j] <= limit){
-                answer++;
                 j++;
-            }else{
                 answer++;
             }
         }
-        return answer;
+        return people.length - answer;
     }
 }
