8a898cffe6b79457f12839ad5cc7361e834efa5d
menzzi <124131845+menzzi@users.noreply.github.com>
2025-01-02 17:46:53 +0900

    [level 1] Title: 최소직사각형, Time: 1.42 ms, Memory: 98.3 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -1,24 +1,27 @@
 class Solution {
     public int solution(int[][] sizes) {
         int answer = 0;
-        for(int i =0;i<sizes.length;i++){
-            if(sizes[i][0]<sizes[i][1]){
+        for(int i = 0; i < sizes.length; i++){
+            if(sizes[i][0] < sizes[i][1]){
                 int tmp = sizes[i][0];
                 sizes[i][0] = sizes[i][1];
                 sizes[i][1] = tmp;
             }
         }
-        int max1 = 0;
-        int max2 = 0;
-        for(int i=0;i<sizes.length;i++){
-            if(max1<sizes[i][0]){
-                max1=sizes[i][0];
+        
+        int maxFirst = 0;
+        int maxSecond = 0;
+        
+        for(int[] size : sizes){
+            if(maxFirst < size[0]){
+                maxFirst = size[0];
             }
-            if(max2<sizes[i][1]){
-                max2=sizes[i][1];
+            if(maxSecond < size[1]){
+                maxSecond = size[1];
             }
         }
-        answer = max1 * max2;
+        
+        answer = maxFirst * maxSecond;
         return answer;
     }
 }
