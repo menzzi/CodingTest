d6ec540446f0b8cc39fc4412910483791da93667
menzzi <124131845+menzzi@users.noreply.github.com>
2025-01-03 14:18:31 +0900

    [level 2] Title: 피로도, Time: 7.18 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,22 +1,21 @@
 import java.util.*;
 
 class Solution {
-    static boolean visited[];
-    static int answer;
-    
+    static int max = -1;
+    static boolean[] check;
     public int solution(int k, int[][] dungeons) {
-        visited = new boolean[dungeons.length];
-        dfs(k,dungeons,0);
-        
-        return answer;
+        check = new boolean[dungeons.length];
+        calculate(k, 0, dungeons);
+        return max;
     }
-    public void dfs(int k,int[][] dungeons,int cnt){
-        for(int i=0;i<dungeons.length;i++){
-            if(visited[i] || k<dungeons[i][0])continue;
-            visited[i] = true;
-            dfs(k-dungeons[i][1],dungeons,cnt+1);
-            visited[i] = false;
+    
+    private void calculate(int k, int count, int[][] dungeons) {
+        for(int i = 0;i < dungeons.length ; i++){
+            if(k < dungeons[i][0] || check[i]) continue;
+            check[i] = true;
+            calculate(k - dungeons[i][1], count + 1, dungeons);
+            check[i] = false;
         }
-        answer = Math.max(answer,cnt);
+        max = Math.max(max, count);
     }
 }
