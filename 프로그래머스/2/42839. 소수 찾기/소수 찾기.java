d9f7ec2cfbbfbae7c7475a1b8cee333560b7f51a
menzzi <124131845+menzzi@users.noreply.github.com>
2025-01-03 13:21:31 +0900

    [level 2] Title: 소수 찾기, Time: 25.89 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -16,10 +16,14 @@ class Solution {
     private boolean isPrime(int number){
         if(number < 2) return false;
         if(number < 4) return true;
-        if (number % 2 == 0 || number % 3 == 0) return false;
+//         if (number % 2 == 0 || number % 3 == 0) return false;
         
-        for(int i = 5; i * i <= number; i+=6){
-            if(number % i == 0 || number % (i+2) == 0) return false;
+//         for(int i = 5; i * i <= number; i+=6){
+//             if(number % i == 0 || number % (i+2) == 0) return false;
+//         }
+        
+        for (int i = 2; i * i <= number; i++) {
+            if (number % i == 0) return false;
         }
         return true;
     }
