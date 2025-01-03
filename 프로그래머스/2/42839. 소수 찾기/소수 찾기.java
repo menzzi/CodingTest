cca565a179f02bf7097a70960e0ae32976dfd29a
menzzi <124131845+menzzi@users.noreply.github.com>
2025-01-03 13:18:35 +0900

    [level 2] Title: 소수 찾기, Time: 37.89 ms, Memory: 94.5 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,30 +1,44 @@
-import java.util.HashSet;
-import java.util.Iterator;
+import java.util.*;
+
 class Solution {
-    HashSet<Integer> set = new HashSet<>();
+    static HashSet<Integer> set = new HashSet<>();
+    static boolean[] check = new boolean[7];
+    
     public int solution(String numbers) {
-        int answer = 0;
-        rec("",numbers);
-        
-        Iterator<Integer> it = set.iterator();
-        while(it.hasNext()){
-            int number = it.next();
-            if(prime(number))answer++;
+        String tmp = "";
+        for(int i = 1; i <= numbers.length(); i++){
+            makeString(numbers,tmp,i);
         }
+        int answer = set.size();
         return answer;
     }
-    public void rec(String tmp,String s){
-        if(!tmp.equals(""))set.add(Integer.parseInt(tmp));
-        for(int i=0;i<s.length();i++){
-            rec(tmp+s.charAt(i),s.substring(0,i)+s.substring(i+1));
-        }
-    }
-    public boolean prime(int num){
-        if(num == 0 || num == 1) return false;
-        int n = (int)Math.sqrt(num);
-        for(int i = 2;i<=n;i++){
-            if(num%i==0)return false;
+    
+    private boolean isPrime(int number){
+        if(number < 2) return false;
+        if(number < 4) return true;
+        if (number % 2 == 0 || number % 3 == 0) return false;
+        
+        for(int i = 5; i * i <= number; i+=6){
+            if(number % i == 0 || number % (i+2) == 0) return false;
         }
         return true;
+    }
+    
+    private void makeString(String n, String tmp, int length){
+        if(tmp.length() == length){
+            if(isPrime(Integer.parseInt(tmp))){
+                set.add(Integer.parseInt(tmp));
+            }
+            return;
+        }
+        
+        for(int j = 0; j < n.length(); j++){
+            if(check[j]) continue;
+            tmp += n.charAt(j);
+            check[j] = true;
+            makeString(n,tmp,length);
+            check[j] = false;
+            tmp = tmp.substring(0,tmp.length()-1);
+        }
     }
 }
