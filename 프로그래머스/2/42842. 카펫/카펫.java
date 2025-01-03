f60c2ed3c9798b40b70e702593fb59f6258a60d7
menzzi <124131845+menzzi@users.noreply.github.com>
2025-01-03 13:39:07 +0900

    [level 2] Title: 카펫, Time: 0.05 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,12 +1,14 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
-        for(int i=3;i<=brown+yellow;i++){
-            int w = (brown+yellow)/i;
-            if((w-2)*(i-2)==yellow){
-                answer[0] = w;
-                answer[1] = i;
-                break;
+        for(int i = 1; i <= yellow; i++){
+            if(yellow % i == 0){
+                int num = ((yellow / i) + i) * 2 + 4;
+                if(num == brown){
+                    answer[0] = (yellow / i) + 2;
+                    answer[1] = i + 2;
+                    return answer;
+                }
             }
         }
         return answer;
