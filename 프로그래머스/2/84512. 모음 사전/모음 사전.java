bf6b6cada3ee844d08d34c13ea7eb099360a1e40
menzzi <124131845+menzzi@users.noreply.github.com>
2025-01-04 13:58:43 +0900

    [level 2] Title: 모음 사전, Time: 1.86 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,14 +1,28 @@
 import java.util.*;
 
 class Solution {
+    static String[] alphabet = {"A","E","I","O","U"};
+    static int count = 0;
+    static int answer = 0;
     public int solution(String word) {
-        int answer = word.length();
-        String str = "AEIOU";
-        int[] num = {781,156,31,6,1};
-        for(int i=0;i<word.length();i++){
-            int cnt = str.indexOf(word.charAt(i));
-            answer += num[i]*cnt;
-        }
+        StringBuilder sb = new StringBuilder();
+        recur(sb, word);
         return answer;
+    }
+    
+    private void recur(StringBuilder sb, String word) {
+        if(sb.toString().equals(word)) {
+            answer = count;
+            return;
+        }
+        
+        if(sb.length() == 5) return;
+        
+        for(int i = 0; i < alphabet.length; i++){
+            sb.append(alphabet[i]);
+            count++;
+            recur(sb, word);
+            sb.deleteCharAt(sb.length() - 1);
+        }
     }
 }
