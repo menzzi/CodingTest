95ed0ee2e2f377e1e95a4ccc9f22847240565dab
menzzi <124131845+menzzi@users.noreply.github.com>
2025-01-05 16:06:59 +0900

    [level 3] Title: N으로 표현, Time: 42.17 ms, Memory: 93.7 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -2,27 +2,30 @@ import java.util.*;
 
 class Solution {
     public int solution(int N, int number) {
-        if(N==number) return 1;
-        List<Set<Integer>> dp = new ArrayList<>();
-        for(int i=0;i<=8;i++){
-            dp.add(new TreeSet<>());
+        if(N == number) return 1;
+        
+        List<Set<Integer>> list = new ArrayList<>();
+        
+        for(int i = 0; i <= 8; i++){
+            list.add(new TreeSet<>());
         }
-        dp.get(1).add(N);
-        for(int i=2;i<=8;i++){
-            dp.get(i).add(Integer.parseInt(Integer.toString(N).repeat(i)));
-            for(int j=1;j<i;j++){
-                int k = i-j;
-                for(int num1:dp.get(j)){
-                    for(int num2:dp.get(k)){
-                        dp.get(i).add(num1+num2);
-                        dp.get(i).add(num1-num2);
-                        dp.get(i).add(num1*num2);
-                        if(num2!=0)dp.get(i).add(num1/num2);
+        
+        list.get(1).add(N);
+        
+        for(int i = 2; i <= 8; i++) { 
+            list.get(i).add(Integer.parseInt(Integer.toString(N).repeat(i)));
+            for(int j = 1; j < i; j++) {
+                int k = i - j;
+                for(int num1 : list.get(j)) {
+                    for(int num2 : list.get(k)) {
+                        list.get(i).add(num1 + num2);
+                        list.get(i).add(num1 - num2);
+                        list.get(i).add(num1 * num2);
+                        if(num2 != 0) list.get(i).add(num1 / num2);
                     }
                 }
-                
             }
-            if(dp.get(i).contains(number))return i;
+            if(list.get(i).contains(number)) return i;
         }
         return -1;
     }
