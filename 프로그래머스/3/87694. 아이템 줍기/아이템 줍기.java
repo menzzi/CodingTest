ceef57ac4b140ea2136eb23ed46ac37519e36203
menzzi <124131845+menzzi@users.noreply.github.com>
2025-01-05 14:29:29 +0900

    [level 3] Title: 아이템 줍기, Time: 0.97 ms, Memory: 87.4 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
@@ -1,64 +1,56 @@
 import java.util.*;
 
 class Solution {
-    static int[][] map;
-    static int[][] visited;
-    static int[][] count;
-    static int[] x = {1,0,-1,0};
-    static int[] y = {0,1,0,-1};
+    static final int[] dx = {1, 0, -1, 0};
+    static final int[] dy = {0, 1, 0, -1};
     
+    static int[][] map = new int[101][101];
+    static boolean[][] visited = new boolean[101][101];
+        
     public int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
-        int answer = 0;
-        map = new int[102][102];
-        visited = new int[102][102];
-        count = new int[102][102];
-        characterX = characterX*2;
-        characterY = characterY*2;
-        itemX = itemX*2;
-        itemY = itemY*2;
-        
-        for(int i=0;i<rectangle.length;i++){
-            for(int x=rectangle[i][0]*2;x<=rectangle[i][2]*2;x++){
-                for(int y=rectangle[i][1]*2;y<=rectangle[i][3]*2;y++){
-                    map[x][y] = 1;
-                }
-            }
+        for(int i = 0; i < rectangle.length ; i++) {
+            makeMap(rectangle[i][0]*2, rectangle[i][1]*2, rectangle[i][2]*2, rectangle[i][3]*2);
         }
-        for(int i=0;i<rectangle.length;i++){
-            for(int x=rectangle[i][0]*2+1;x<rectangle[i][2]*2;x++){
-                for(int y=rectangle[i][1]*2+1;y<rectangle[i][3]*2;y++){
-                    map[x][y] = 0;
-                }
-            }
-        }
-        visited[characterX][characterY] = 1;
-        count[characterX][characterY] = 0;
-        answer = BFS(characterX,characterY,itemX,itemY);
-        return answer;
+        return BFS(characterX*2, characterY*2, itemX*2, itemY*2);
     }
-    public int BFS(int cx,int cy, int ix,int iy){
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{cx,cy});
+    
+    private int BFS(int characterX, int characterY, int itemX, int itemY) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{characterX, characterY, 0});
         
-        while(!q.isEmpty()){
-            int[] n = q.remove();
-            int nx = n[0];
-            int ny = n[1];
-            if(nx == ix && ny == iy) return count[nx][ny]/2;
+        while(!queue.isEmpty()) {
+            int[] poll = queue.poll();
+            int x = poll[0];
+            int y = poll[1];
+            int cnt = poll[2];
             
-            for(int i=0;i<4;i++){
-                int dx = nx + x[i];
-                int dy = ny + y[i];
+            if(x == itemX && y == itemY) return cnt / 2;
+            
+            for(int i = 0; i < 4; i++) {
+                int cx = x + dx[i];
+                int cy = y + dy[i];
                 
-                if(dx>1&&dx<=100&&dy>1&&dy<=100){
-                    if(visited[dx][dy]==0 && map[dx][dy]==1){
-                        q.add(new int[]{dx,dy});
-                        count[dx][dy] = count[nx][ny]+1;
-                        visited[dx][dy] = 1;
-                    }
+                if(cx > 100 || cx < 0 || cy > 100 || cy < 0) continue;
+                
+                if(!visited[cy][cx] && map[cy][cx] == 2) {
+                    visited[cy][cx] = true;
+                    queue.add(new int[]{cx,cy,cnt+1});
                 }
             }
         }
-        return 0;
+        return -1;
+    }
+    
+    private void makeMap(int x1, int y1, int x2, int y2) {
+        for(int i = y1; i <= y2; i++) {
+            for(int j = x1; j <= x2; j++) {
+                if(map[i][j] == 1) continue;
+                if(i == y1 || i == y2 || j == x1 || j == x2) {
+                    map[i][j] = 2;
+                    continue;
+                }
+                map[i][j] = 1;
+            }
+        }
     }
 }
