325e58f33cafe93b31391d169ba9ba96fcdc07d1
menzzi <124131845+menzzi@users.noreply.github.com>
2025-01-06 16:21:34 +0900

    [level 3] Title: 정수 삼각형, Time: 10.23 ms, Memory: 63.2 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -3,20 +3,19 @@ import java.util.*;
 class Solution {
     public int solution(int[][] triangle) {
         int answer = 0;
-        int[][] map = new int[triangle.length][triangle.length];
-        map[0][0] = triangle[0][0];
-        
-        for(int i=1;i<triangle.length;i++){
-            map[i][0] = map[i-1][0] + triangle[i][0];
-            for(int j=1;j<triangle[i].length-1;j++){
-                map[i][j] = Math.max(map[i-1][j-1],map[i-1][j]) + triangle[i][j];
+        int[][] sum = new int[triangle.length][triangle.length]; 
+        sum[0][0] = triangle[0][0];
+        for(int i = 1; i < triangle.length; i++) {
+            sum[i][0] = triangle[i][0] + sum[i-1][0];
+            for(int j = 1; j < triangle[i].length - 1; j++) {
+                sum[i][j] = Math.max(sum[i-1][j-1], sum[i-1][j]) + triangle[i][j];
             }
-            map[i][triangle[i].length-1] = map[i-1][triangle[i-1].length-1] + triangle[i][triangle[i].length-1];
-
+            sum[i][triangle[i].length - 1] = triangle[i][triangle[i].length - 1] + sum[i-1][triangle[i-1].length - 1];
+            
         }
         
-        for(int i=0;i<triangle.length;i++){
-            answer = Math.max(answer,map[triangle.length-1][i]);
+        for(int i = 0; i < sum.length - 1; i++){
+            answer = Math.max(sum[sum.length - 1][i], answer);
         }
         return answer;
     }
