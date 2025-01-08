eea0e04ee370d2763878021cf4b519978fa7549f
menzzi <124131845+menzzi@users.noreply.github.com>
2025-01-08 18:01:07 +0900

    [level 3] Title: 네트워크, Time: 0.25 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,24 +1,25 @@
 class Solution {
-    boolean[] visited;
+    static boolean[] visited;
+    
     public int solution(int n, int[][] computers) {
         int answer = 0;
         visited = new boolean[n];
         
         for(int i = 0; i < n; i++) {
             if(!visited[i]){
-                answer++;
                 DFS(n, computers, i);
+                answer++;
             }
         }
+        
         return answer;
     }
     
-    private void DFS(int n, int[][] computers, int index) {
+    public void DFS(int n, int[][] computers, int index) {
         visited[index] = true;
-        
         for(int i = 0; i < n; i++){
-            if(!visited[i] && computers[index][i] == 1){
-                DFS(n,computers,i);
+            if(computers[index][i] == 1 && !visited[i]){
+                DFS(n, computers, i);
             }
         }
     }
