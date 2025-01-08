cc65ee33823f9b1a0f051aeef2ac6e2715d7f04e
menzzi <124131845+menzzi@users.noreply.github.com>
2025-01-08 15:28:26 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 18.77 ms, Memory: 91.2 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,47 +1,47 @@
 import java.util.*;
 
 class Solution {
-    static int[][] map; 
-    static boolean[] visited;
     public int solution(int n, int[][] wires) {
         int answer = n;
-        
-        map = new int[n+1][n+1];
+        int[][] map = new int[n+1][n+1];
         
         for(int i = 0; i < wires.length; i++){
             map[wires[i][0]][wires[i][1]] = 1;
             map[wires[i][1]][wires[i][0]] = 1;
         }
         
-       for(int i = 0; i < wires.length; i++){
-            map[wires[i][0]][wires[i][1]] = 0;
-            map[wires[i][1]][wires[i][0]] = 0;
-            answer = Math.min(answer, findConnect(n, wires[i][0]));
-            map[wires[i][0]][wires[i][1]] = 1;
-            map[wires[i][1]][wires[i][0]] = 1;
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n; j++){
+                if(map[i][j] == 1){
+                    map[i][j] = 0;
+                    map[j][i] = 0;
+                    answer = Math.min(answer, calculateWire(map, i, n));
+                    map[i][j] = 1;
+                    map[j][i] = 1;
+                }
+            }
         }
-        
         return answer;
     }
     
-    private int findConnect(int n, int wire){
+    private int calculateWire(int[][] map, int num, int n) {
         Queue<Integer> queue = new LinkedList<>();
-        queue.add(wire);
+        queue.add(num);
         
-        visited = new boolean[n+1];
+        boolean[] visited = new boolean[n+1];
+        visited[num] = true;
         int count = 1;
         
         while(!queue.isEmpty()){
             int poll = queue.poll();
-            visited[poll] = true;
-            for(int i = 1; i <= n ;i++){
-                if(visited[i])continue;
-                if(map[poll][i] == 1){
-                    queue.add(i);
+            for(int i = 1; i <= n; i++){
+                if(!visited[i] && map[poll][i] == 1){
                     count++;
+                    visited[i] = true;
+                    queue.add(i);
                 }
             }
         }
-        return Math.abs(n - 2*count);
+        return Math.abs(n - (2 * count));
     }
 }
