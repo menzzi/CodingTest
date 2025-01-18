b43fe7ff9c9e35cf0e9212a3092e3168e8089c9b
menzzi <124131845+menzzi@users.noreply.github.com>
2025-01-18 18:28:32 +0900

    [level 3] Title: 아이템 줍기, Time: 0.80 ms, Memory: 80.4 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
@@ -4,53 +4,52 @@ class Solution {
     static final int[] dx = {1, 0, -1, 0};
     static final int[] dy = {0, 1, 0, -1};
     
-    static int[][] map = new int[101][101];
-    static boolean[][] visited = new boolean[101][101];
-        
     public int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
-        for(int i = 0; i < rectangle.length ; i++) {
-            makeMap(rectangle[i][0]*2, rectangle[i][1]*2, rectangle[i][2]*2, rectangle[i][3]*2);
+        int[][] map = new int[101][101];
+        
+        for(int i = 0; i < rectangle.length; i++) {
+            int x1 = rectangle[i][0] * 2;
+            int y1 = rectangle[i][1] * 2;
+            int x2 = rectangle[i][2] * 2;
+            int y2 = rectangle[i][3] * 2;
+            
+            for(int x = x1; x <= x2; x++) {
+                for(int y = y1; y <= y2; y++) {
+                    if(map[x][y] == -1) continue;
+                    if(x == x1 || x == x2 || y == y1 || y == y2) {
+                        map[x][y] = 1;
+                        continue;
+                    }
+                    map[x][y] = -1;
+                }
+            }
         }
-        return BFS(characterX*2, characterY*2, itemX*2, itemY*2);
-    }
-    
-    private int BFS(int characterX, int characterY, int itemX, int itemY) {
+        
         Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{characterX, characterY, 0});
+        queue.add(new int[]{characterX * 2, characterY * 2, 0});
+        
+        boolean[][] visited = new boolean[101][101];
         
         while(!queue.isEmpty()) {
             int[] poll = queue.poll();
             int x = poll[0];
             int y = poll[1];
             int cnt = poll[2];
-            
-            if(x == itemX && y == itemY) return cnt / 2;
+            if(x == itemX * 2 && y == itemY * 2) return cnt / 2;
             
             for(int i = 0; i < 4; i++) {
-                int cx = x + dx[i];
-                int cy = y + dy[i];
+                int nx = x + dx[i];
+                int ny = y + dy[i];
                 
-                if(cx > 100 || cx < 0 || cy > 100 || cy < 0) continue;
+                if(nx < 0 || ny < 0 || nx > 100 || ny > 100) continue;
                 
-                if(!visited[cy][cx] && map[cy][cx] == 2) {
-                    visited[cy][cx] = true;
-                    queue.add(new int[]{cx,cy,cnt+1});
+                if(!visited[nx][ny] && map[nx][ny] == 1){
+                    visited[nx][ny] = true;
+                    queue.add(new int[]{nx, ny, cnt + 1});
                 }
             }
+            
         }
         return -1;
-    }
-    
-    private void makeMap(int x1, int y1, int x2, int y2) {
-        for(int i = y1; i <= y2; i++) {
-            for(int j = x1; j <= x2; j++) {
-                if(map[i][j] == 1) continue;
-                if(i == y1 || i == y2 || j == x1 || j == x2) {
-                    map[i][j] = 2;
-                    continue;
-                }
-                map[i][j] = 1;
-            }
-        }
     }
 }
