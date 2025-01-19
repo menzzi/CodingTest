76125f6f79b0927682c9e28a5bf4cf1394876137
menzzi <124131845+menzzi@users.noreply.github.com>
2025-01-19 13:34:41 +0900

    [level 3] Title: N으로 표현, Time: 60.50 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -3,30 +3,30 @@ import java.util.*;
 class Solution {
     public int solution(int N, int number) {
         if(N == number) return 1;
+        List<Set<Integer>> result = new ArrayList<>();
         
-        List<Set<Integer>> list = new ArrayList<>();
-        
-        for(int i = 0; i <= 8; i++){
-            list.add(new TreeSet<>());
+        for(int i = 0; i <= 8; i++) {
+            result.add(new TreeSet<>());
         }
         
-        list.get(1).add(N);
+        result.get(1).add(N);
         
-        for(int i = 2; i <= 8; i++) { 
-            list.get(i).add(Integer.parseInt(Integer.toString(N).repeat(i)));
+        for(int i = 2; i < 9; i++) {
+            result.get(i).add(Integer.parseInt(Integer.toString(N).repeat(i)));
             for(int j = 1; j < i; j++) {
                 int k = i - j;
-                for(int num1 : list.get(j)) {
-                    for(int num2 : list.get(k)) {
-                        list.get(i).add(num1 + num2);
-                        list.get(i).add(num1 - num2);
-                        list.get(i).add(num1 * num2);
-                        if(num2 != 0) list.get(i).add(num1 / num2);
+                for(int num1 : result.get(j)) {
+                    for(int num2 : result.get(k)) {
+                        result.get(i).add(num1 - num2);
+                        result.get(i).add(num1 + num2);
+                        result.get(i).add(num1 * num2);
+                        if(num2 != 0) result.get(i).add(num1 / num2);
                     }
                 }
             }
-            if(list.get(i).contains(number)) return i;
+            if(result.get(i).contains(number)) return i;
         }
+        
         return -1;
     }
 }
