f5fea256c6505874d6209362d94a57edd7078e87
menzzi <124131845+menzzi@users.noreply.github.com>
2025-01-19 18:59:46 +0900

    [level 4] Title: 도둑질, Time: 21.34 ms, Memory: 105 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/도둑질.java
+++ 프로그래머스/4/42897. 도둑질/도둑질.java
@@ -3,9 +3,10 @@ import java.util.*;
 class Solution {
     public int solution(int[] money) {
         int answer = 0;
-        if(money.length == 3){
+        if(money.length == 3) {
             return Math.max(money[0], Math.max(money[1], money[2]));
         }
+        
         int[] left = new int[money.length];
         int[] right = new int[money.length];
         
@@ -15,12 +16,14 @@ class Solution {
         right[0] = 0;
         right[1] = money[1];
         
-        for(int i = 2; i < money.length; i++){
-            right[i] = Math.max(right[i-1],right[i-2] + money[i]);
-            if(i == money.length - 1)break;
-            left[i] = Math.max(left[i-1],left[i-2] + money[i]);
+        for(int i = 2; i < money.length; i++) {
+            right[i] = Math.max(right[i - 1], right[i - 2] + money[i]);
+            answer = Math.max(answer, right[i]);
+            if(i == money.length - 1) continue;
+            left[i] = Math.max(left[i - 1], left[i - 2] + money[i]);
+            answer = Math.max(answer, left[i]);
         }
-        answer = Math.max(right[money.length - 1], left[money.length - 2]);
+        
         return answer;
     }
 }
