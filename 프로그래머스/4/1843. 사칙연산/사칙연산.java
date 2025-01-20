d250a3322b91bbb96a69bbb892ada0a05f09c67e
menzzi <124131845+menzzi@users.noreply.github.com>
2025-01-20 16:17:50 +0900

    [level 4] Title: 사칙연산, Time: 20.56 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/4/1843. 사칙연산/사칙연산.java
+++ 프로그래머스/4/1843. 사칙연산/사칙연산.java
@@ -1,39 +1,36 @@
-import java.util.*;
-
 class Solution {
     public int solution(String arr[]) {
         int size = arr.length / 2 + 1;
-        int[][] maxMap = new int[size][size];
-        int[][] minMap = new int[size][size];
+        int[][] max = new int[size][size];
+        int[][] min = new int[size][size];
         
         for(int i = 0; i < size; i++) {
-            maxMap[i][i] = Integer.parseInt(arr[i * 2]);
-            minMap[i][i] = Integer.parseInt(arr[i * 2]);
+            max[i][i] = Integer.parseInt(arr[i * 2]);
+            min[i][i] = Integer.parseInt(arr[i * 2]);
         }
         
         for(int len = 1; len < size; len++) {
-            for(int i = 0; i < size - len ; i++) {
+            for(int i = 0; i < size - len; i++) {
                 int j = i + len;
-                maxMap[i][j] = Integer.MIN_VALUE;
-                minMap[i][j] = Integer.MAX_VALUE;
-                
+                max[i][j] = Integer.MIN_VALUE;
+                min[i][j] = Integer.MAX_VALUE;
                 for(int k = i; k < j; k++) {
-                    String operator = arr[k * 2 + 1];
-                    int maxLeft = maxMap[i][k];
-                    int minLeft = minMap[i][k];
-                    int maxRight = maxMap[k + 1][j];
-                    int minRight = minMap[k + 1][j];
+                    String op = arr[k * 2 + 1];
+                    int maxL = max[i][k];
+                    int minL = min[i][k];
+                    int maxR = max[k + 1][j];
+                    int minR = min[k + 1][j];
                     
-                    if(operator.equals("+")){
-                        maxMap[i][j] = Math.max(maxMap[i][j], maxLeft + maxRight);
-                        minMap[i][j] = Math.min(minMap[i][j], minLeft + minRight);
-                    }else{
-                        maxMap[i][j] = Math.max(maxMap[i][j], maxLeft - minRight);
-                        minMap[i][j] = Math.min(minMap[i][j], minLeft - maxRight);
+                    if(op.equals("+")) {
+                        max[i][j] = Math.max(max[i][j], maxL + maxR);
+                        min[i][j] = Math.min(min[i][j], minL + minR);
+                    }else {
+                        max[i][j] = Math.max(max[i][j], maxL - minR);
+                        min[i][j] = Math.min(min[i][j], minL - maxR);
                     }
                 }
             }
         }
-        return maxMap[0][size-1];
+        return max[0][size - 1];
     }
 }
