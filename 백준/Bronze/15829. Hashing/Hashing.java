e854364a3e1984f88cb2bf2408cbf6be97d7ec69
menzzi <124131845+menzzi@users.noreply.github.com>
2025-01-24 19:33:03 +0900

    [Bronze II] Title: Hashing, Time: 108 ms, Memory: 14228 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -7,28 +7,18 @@ class Main {
         int N = Integer.parseInt(br.readLine());
         String str = br.readLine();
         long answer = 0;
+        long pow = 1;
 
         for(int i = 0; i < N; i++) {
             int num = convertToInt(str.charAt(i));
-            long powNum = pow(31, i);
+            answer = (answer + ((long)num * pow)) % 1234567891;
 
-            answer += ((long)num * powNum) % 1234567891;
+            pow = (pow * 31) % 1234567891;
         }
         System.out.println(answer);
     }
 
     private static int convertToInt(char a) {
         return a - 'a' + 1;
-    }
-
-    private static long pow(int r, int index) {
-        if(index == 0) return 1;
-        long res = pow(r, index/2);
-
-        if(index % 2 == 0) {
-            return res * res;
-        }else {
-            return res * res * r;
-        }
     }
 }
