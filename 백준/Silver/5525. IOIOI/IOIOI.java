4dec3f4a58d99543deb9790671af237e90f7372f
menzzi <124131845+menzzi@users.noreply.github.com>
2025-01-24 17:21:40 +0900

    [Silver I] Title: IOIOI, Time: 240 ms, Memory: 20232 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -8,21 +8,21 @@ class Main {
         int M = Integer.parseInt(br.readLine());
         String s = br.readLine();
 
-        String pn = makePn(N);
-
-        int num = (N * 2) + 1;
         int answer = 0;
-        for(int i = 0; i <= M - num; i++) {
-            if(pn.equals(s.substring(i,i + num))) answer++;
+        int count = 0;
+        for(int i = 0; i < M-2; i++) {
+            if(s.charAt(i) == 'I' && s.charAt(i+1) == 'O' && s.charAt(i+2) == 'I') {
+                count++;
+                i++;
+
+                if(count == N) {
+                    answer++;
+                    count--;
+                }
+            } else {
+                count = 0;
+            }
         }
         System.out.println(answer);
-    }
-
-    private static String makePn(int N) {
-        StringBuilder sb = new StringBuilder();
-        sb.append("I");
-        String str = "OI";
-        sb.append(str.repeat(N));
-        return sb.toString();
     }
 }
