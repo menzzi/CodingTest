6da8190af4e0048e952c3b97b5bd4daee217c72f
menzzi <124131845+menzzi@users.noreply.github.com>
2025-01-26 20:13:24 +0900

    [Silver IV] Title: 제로, Time: 212 ms, Memory: 23864 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -1,31 +1,24 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.Stack;
+import java.util.*;
+import java.io.*;
 
-public class Main {
+class Main {
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        Stack<Integer> st = new Stack<>();
-
         int N = Integer.parseInt(br.readLine());
-        int result = 0;
+        Stack<Integer> stack = new Stack<>();
 
-        for(int i=0;i<N;i++){
-            int num = Integer.parseInt(br.readLine());
-            if(num==0){
-                st.pop();
-            }else{
-                st.push(num);
+        for (int i = 0; i < N; i++) {
+            int number = Integer.parseInt(br.readLine());
+            if (number == 0) {
+                stack.pop();
+            }else {
+                stack.push(number);
             }
         }
-        while(!st.isEmpty()){
-            result += st.pop();
+        int answer = 0;
+        while (!stack.isEmpty()) {
+            answer += stack.pop();
         }
-
-        System.out.println(result);
+        System.out.println(answer);
     }
 }
