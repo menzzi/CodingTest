f673e8a35c985ff9efd5974252af95a25594540a
menzzi <124131845+menzzi@users.noreply.github.com>
2025-02-14 17:12:46 +0900

    [Gold IV] Title: 트리의 지름, Time: 2920 ms, Memory: 129664 KB -BaekjoonHub

--- 백준/Gold/1967. 트리의 지름/트리의 지름.java
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.java
@@ -2,44 +2,43 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-    static int answer = 0;
-    static ArrayList<int[]>[] tree;
     static boolean[] visited;
-
-    public static void main(String[] args) throws IOException{
+    static int n, answer = 0;
+    static ArrayList<int[]>[] tree;
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int nodeCount = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
 
-        tree = new ArrayList[nodeCount + 1];
-
-        for(int i = 1; i <= nodeCount; i++) {
+        tree = new ArrayList[n + 1];
+        for(int i = 1; i <= n; i++) {
             tree[i] = new ArrayList<>();
         }
 
-        String line;
-        while((line = br.readLine())!= null) {
-            String[] values = line.split(" ");
-            int from = Integer.parseInt(values[0]);
-            int to = Integer.parseInt(values[1]);
-            int value = Integer.parseInt(values[2]);
+        StringTokenizer st;
+        for(int i = 0; i < n - 1; i++) {
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            int value = Integer.parseInt(st.nextToken());
 
             tree[from].add(new int[] {to, value});
             tree[to].add(new int[] {from, value});
         }
 
-        for(int i = 1; i<= nodeCount; i++) {
-            visited = new boolean[nodeCount + 1];
+        for(int i = 1; i <= n; i++){
+            visited = new boolean[n + 1];
             visited[i] = true;
             DFS(i, 0);
         }
 
         System.out.println(answer);
     }
-    private static void DFS(int idx, int sum) {
-        visited[idx] = true;
-        answer = Math.max(answer, sum);
 
-        for(int[] node : tree[idx]) {
+    private static void DFS(int index, int sum) {
+        visited[index] = true;
+        answer = Math.max(sum, answer);
+
+        for(int[] node : tree[index]) {
             if(!visited[node[0]]) {
                 DFS(node[0], sum + node[1]);
             }
