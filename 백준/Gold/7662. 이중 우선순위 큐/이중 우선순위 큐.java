c41b82b01c81a5f72cd908a44e1522380cacc90b
menzzi <124131845+menzzi@users.noreply.github.com>
2025-02-14 16:53:00 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 2700 ms, Memory: 441668 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
@@ -1,34 +1,36 @@
 import java.util.*;
 import java.io.*;
 
-
 class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int number = Integer.parseInt(br.readLine());
+        int T = Integer.parseInt(br.readLine());
 
-        for(int i = 0; i < number; i++) {
-            PriorityQueue<Integer> minQueue = new PriorityQueue<>();
-            PriorityQueue<Integer> maxQueue = new PriorityQueue<>(Collections.reverseOrder());
-            HashMap<Integer,Integer> map = new HashMap<>();
+        while(T > 0) {
+            T--;
+            int k = Integer.parseInt(br.readLine());
+            PriorityQueue<Integer> minQ = new PriorityQueue<>();
+            PriorityQueue<Integer> maxQ = new PriorityQueue<>(Collections.reverseOrder());
+            Map<Integer, Integer> map = new HashMap<>();
             int queueSize = 0;
-            int num = Integer.parseInt(br.readLine());
+            for(int i = 0; i < k; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                String str1 = st.nextToken();
 
-            for(int j = 0; j < num; j++) {
-                String[] str = br.readLine().split(" ");
-                if(str[0].equals("I")) {
-                    int input = Integer.parseInt(str[1]);
-                    minQueue.add(input);
-                    maxQueue.add(input);
+                if(str1.equals("I")) {
+                    int number = Integer.parseInt(st.nextToken());
+                    minQ.add(number);
+                    maxQ.add(number);
+                    map.put(number, map.getOrDefault(number, 0) + 1);
                     queueSize++;
-                    map.put(input, map.getOrDefault(input,0) + 1);
                 } else {
                     if(queueSize == 0) continue;
-                    if(str[1].equals("1")) {
-                        delete(maxQueue, map);
+                    String str2 = st.nextToken();
+                    if(str2.equals("1")) {
+                        delete(maxQ, map);
                         queueSize--;
                     } else {
-                        delete(minQueue, map);
+                        delete(minQ, map);
                         queueSize--;
                     }
                 }
@@ -37,20 +39,21 @@ class Main {
             if(queueSize == 0) {
                 System.out.println("EMPTY");
             } else {
-                int max = delete(maxQueue, map);
+                int max = delete(maxQ, map);
                 int min = max;
-                if(queueSize > 1){
-                    min = delete(minQueue, map);
-                } 
+                if(queueSize > 1) {
+                    min = delete(minQ, map);
+                }
                 System.out.printf("%d %d\n", max, min);
             }
         }
     }
-    private static int delete(PriorityQueue<Integer> queue, HashMap<Integer, Integer> map) {
+
+    private static int delete(PriorityQueue<Integer> queue, Map<Integer, Integer> map) {
         int result = 0;
         while(!queue.isEmpty()) {
             int poll = queue.poll();
-            int cnt = map.getOrDefault(poll,0);
+            int cnt = map.getOrDefault(poll, 0);
             if(cnt == 0) continue;
             if(cnt == 1) map.remove(poll);
             else map.put(poll, cnt - 1);
