6974de5a8d8726015737b096c2478af0359e363e
menzzi <124131845+menzzi@users.noreply.github.com>
2025-02-15 16:44:56 +0900

    [Silver II] Title: 랜선 자르기, Time: 152 ms, Memory: 17296 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -2,38 +2,35 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-    public static void main(String[] args) throws IOException{
+    static int K, N;
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int K = Integer.parseInt(st.nextToken());
-        int N = Integer.parseInt(st.nextToken());
-
-        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        K = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        int[] arr = new int[K];
         long max = 0;
         for(int i = 0; i < K; i++) {
             arr[i] = Integer.parseInt(br.readLine());
 
-            if(arr[i] > max) {
-                max = arr[i];
-            }
+            max = Math.max(max, arr[i]);
         }
-
         max++;
-        
+
         long min = 0;
         long mid = 0;
-        
         while(min < max) {
             mid = (min + max) / 2;
             long count = 0;
-            for(int i = 0;i < N; i++) {
+
+            for(int i = 0; i < K; i++) {
                 count += (arr[i] / mid);
             }
 
-            if(count < N) {
-                max = mid;
-            } else {
+            if(count >= N) {
                 min = mid + 1;
+            } else {
+                max = mid;
             }
         }
 
