26bd832fcd1d8e62d949af16f9ea66acf0345ac9
menzzi <124131845+menzzi@users.noreply.github.com>
2025-02-15 18:33:13 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 1696 ms, Memory: 75672 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
@@ -2,44 +2,47 @@ import java.util.*;
 import java.io.*;
 
 class Main {
+    static int N, M;
     static int[][] map;
     static int[][] count;
     static boolean[][] visited;
 
-    static final int[] dx = {1, 0, -1, 0};
-    static final int[] dy = {0, 1, 0, -1};
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        map = new int[n][m];
-        count = new int[n][m];
-        visited = new boolean[n][m];
-        int x = 0, y = 0;
+        map = new int[N][M];
+        count = new int[N][M];
+        visited = new boolean[N][M];
 
-        for(int i = 0; i < n ; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            for(int j = 0; j < m; j++) {
+        int[] target = new int[2];
+
+        for(int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < M; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
+
                 if(map[i][j] == 2) {
-                    x = i;
-                    y = j;
+                    target[0] = i;
+                    target[1] = j;
                 }
             }
         }
 
-        visited[x][y] = true;
-        BFS(x, y);
+        visited[target[0]][target[1]] = true;
+        BFS(target[0], target[1]);
 
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m ; j++) {
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < M; j++) {
                 if(map[i][j] == 0 || map[i][j] == 2) {
-                    System.out.print("0 ");
+                    System.out.print(0 + " ");
                 } else if(!visited[i][j] && map[i][j] == 1) {
-                    System.out.print("-1 ");
+                    System.out.print(-1 + " ");
                 } else {
                     System.out.print(count[i][j] + " ");
                 }
@@ -50,23 +53,23 @@ class Main {
 
     private static void BFS(int x, int y) {
         Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{x,y});
+        queue.add(new int[] {x, y});
 
         while(!queue.isEmpty()) {
             int[] poll = queue.poll();
-            int px = poll[0];
-            int py = poll[1];
+            int cx = poll[0];
+            int cy = poll[1];
 
             for(int i = 0; i < 4; i++) {
-                int nx = px + dx[i];
-                int ny = py + dy[i];
+                int nx = cx + dx[i];
+                int ny = cy + dy[i];
 
-                if(nx < 0 || ny < 0 || nx >= map.length || ny >= map[0].length) continue;
+                if(nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
 
-                if(map[nx][ny] == 1 && !visited[nx][ny]) {
+                if(!visited[nx][ny] && map[nx][ny] == 1) {
                     visited[nx][ny] = true;
-                    queue.add(new int[]{nx,ny});
-                    count[nx][ny] = count[px][py] + 1;
+                    queue.add(new int[] {nx, ny});
+                    count[nx][ny] = count[cx][cy] + 1;
                 }
             }
         }
