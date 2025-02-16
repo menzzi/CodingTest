b9bfabfd8504f210bc184904842946f4be57a9c6
menzzi <124131845+menzzi@users.noreply.github.com>
2025-02-16 16:48:22 +0900

    [Gold IV] Title: 거짓말, Time: 104 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Gold/1043. 거짓말/거짓말.java
+++ 백준/Gold/1043. 거짓말/거짓말.java
@@ -3,23 +3,25 @@ import java.io.*;
 
 class Main {
     static int N, M;
-    static List<Integer> knowTheTruth = new ArrayList<>();
     static int[] parent;
-
+    static List<int[]> parties;
+    static List<Integer> knowTheTruth;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        st = new StringTokenizer(br.readLine(), " ");
-        int number = Integer.parseInt(st.nextToken());
+        parties = new ArrayList<>();
+        knowTheTruth = new ArrayList<>();
 
+        st = new StringTokenizer(br.readLine());
+        int number = Integer.parseInt(st.nextToken());
         if(number == 0) {
             System.out.println(M);
             return;
         }
-
         for(int i = 0; i < number; i++) {
             knowTheTruth.add(Integer.parseInt(st.nextToken()));
         }
@@ -29,34 +31,31 @@ class Main {
             parent[i] = i;
         }
 
-        List<Integer> [] parties = new ArrayList[M];
         for(int i = 0; i < M; i++) {
-            parties[i] = new ArrayList<>();
-        }
-
-        for(int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
+            st = new StringTokenizer(br.readLine());
             int num = Integer.parseInt(st.nextToken());
-            int x = Integer.parseInt(st.nextToken());
-            parties[i].add(x);
+            int[] arr = new int[num];
+            arr[0] = Integer.parseInt(st.nextToken());
             for(int j = 1; j < num; j++) {
-                int y = Integer.parseInt(st.nextToken());
-                union(x,y);
-                parties[i].add(y);
+                arr[j] = Integer.parseInt(st.nextToken());
+                union(arr[0], arr[j]);
             }
+            parties.add(arr);
         }
 
-        int cnt = 0;
+        int count = 0;
         for(int i = 0; i < M; i++) {
+            int[] party = parties.get(i);
             boolean flag = true;
-            for(int num : parties[i]) {
-                if(knowTheTruth.contains(find(parent[num]))){
-                    flag = false;break;
+            for(int num : party) {
+                if(knowTheTruth.contains(find(parent[num]))) {
+                    flag = false;
+                    break;
                 }
             }
-            if(flag) cnt++;
+            if(flag) count++;
         }
-        System.out.println(cnt);
+        System.out.println(count);
     }
 
     private static int find(int x) {
@@ -67,11 +66,13 @@ class Main {
     private static void union(int x, int y) {
         int parentX = find(x);
         int parentY = find(y);
+
         if(knowTheTruth.contains(parentY)) {
             int tmp = parentX;
             parentX = parentY;
             parentY = tmp;
         }
+
         parent[parentY] = parentX;
     }
 }
