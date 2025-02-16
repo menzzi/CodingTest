0e188c4ef0efb678df4438af8b0b9d9989aa87cd
menzzi <124131845+menzzi@users.noreply.github.com>
2025-02-16 18:39:36 +0900

    [Gold III] Title: 아기 상어, Time: 192 ms, Memory: 26420 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.java
+++ 백준/Gold/16236. 아기 상어/아기 상어.java
@@ -2,22 +2,22 @@ import java.util.*;
 import java.io.*;
 
 class Main {
+    static int N, sharkSize = 2, time = 0;
     static int[][] map;
-    static int N, time = 0;
-    static int sharkSize = 2;
 
     static final int[] dx = {1, 0, -1, 0};
     static final int[] dy = {0, 1, 0, -1};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
         N = Integer.parseInt(br.readLine());
 
-        int[] shark = new int[2];
         map = new int[N][N];
+        StringTokenizer st;
+        int[] shark = new int[2];
+
         for(int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
+            st = new StringTokenizer(br.readLine());
             for(int j = 0; j < N; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
 
@@ -41,25 +41,23 @@ class Main {
             boolean[][] visited = new boolean[N][N];
 
             visited[x][y] = true;
-            queue.add(new int[] {x, y, 0});
+            queue.add(new int[] {x,y,0});
 
             while(!queue.isEmpty()) {
                 int[] poll = queue.poll();
-                int moving = poll[2];
-
                 for(int i = 0; i < 4; i++) {
                     int nx = poll[0] + dx[i];
                     int ny = poll[1] + dy[i];
 
                     if(nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
+
                     if(!visited[nx][ny]) {
                         visited[nx][ny] = true;
-
                         if(map[nx][ny] <= sharkSize) {
                             if(map[nx][ny] != 0 && map[nx][ny] < sharkSize) {
-                                fishes.add(new int[]{nx, ny, moving + 1});
+                                fishes.add(new int[] {nx, ny, poll[2] + 1});
                             }
-                            queue.add(new int[]{nx, ny, moving + 1});
+                            queue.add(new int[] {nx, ny, poll[2] + 1});
                         }
                     }
                 }
@@ -67,43 +65,30 @@ class Main {
 
             if(fishes.isEmpty()) {
                 break;
-            } else {
-                if(fishes.size() > 1) {
-                    sorting(fishes);
-                }
+            }
 
-                int[] oneFish = fishes.get(0);
-                time += oneFish[2];
-                eating++;
+            if(fishes.size() > 1) {
+                fishes.sort((a,b) -> {
+                   if(a[2] == b[2]) {
+                       if(a[0] == b[0]) return a[1] - b[1];
+                       else return a[0] - b[0];
+                   } else return a[2] - b[2];
+                });
+            }
 
-                map[x][y] = 0;
-                x = oneFish[0];
-                y = oneFish[1];
-                map[x][y] = 9;
+            int[] fish = fishes.get(0);
+            time += fish[2];
+            eating++;
 
-                if(sharkSize == eating) {
-                    sharkSize++;
-                    eating = 0;
-                }
+            map[x][y] = 0;
+            x = fish[0];
+            y = fish[1];
+            map[x][y] = 9;
+
+            if(eating == sharkSize) {
+                sharkSize++;
+                eating = 0;
             }
         }
-    }
-
-    private static void sorting(List<int[]> list) {
-        Collections.sort(list, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                if(o1[2] == o2[2]) {
-                    if(o1[0] == o2[0]) {
-                        return o1[1] - o2[1];
-
-                    }else {
-                        return o1[0] - o2[0];
-                    }
-                } else{
-                    return o1[2] - o2[2];
-                }
-            }
-        });
     }
 }
